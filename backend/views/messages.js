108d90fcbcf813d0038d094c3607834324ea041b
yasmeinelkenany <yasmeinelkenany@gmail.com>
2024-08-14 01:18:35 +0300

    refactor: moving messages.js file from routes to views

---
+++ backend/views/messages.js
@@ -0,0 +1,14 @@
+const messages = [
+    { id: 0, added: new Date(), text: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.', user: 'Albert Schweitzer' },
+    { id: 1, added: new Date(), text: 'The only way to do great work is to love what you do.', user: 'Steve Jobs' },
+    { id: 2, added: new Date(), text: 'It always seems impossible until it is done.', user: 'Nelson Mandela' },
+    { id: 3, added: new Date(), text: 'Do not wait for the perfect moment, take the moment and make it perfect.', user: 'Unknown' },
+    { id: 4, added: new Date(), text: 'The best way to predict the future is to create it.', user: 'Peter Drucker' },
+    { id: 5, added: new Date(), text: 'You miss 100% of the shots you don’t take.', user: 'Wayne Gretzky' },
+    { id: 6, added: new Date(), text: 'Don’t watch the clock; do what it does. Keep going.', user: 'Sam Levenson' },
+    { id: 7, added: new Date(), text: 'The journey of a thousand miles begins with one step.', user: 'Lao Tzu' },
+    { id: 8, added: new Date(), text: 'Believe you can and you’re halfway there.', user: 'Theodore Roosevelt' },
+    { id: 9, added: new Date(), text: 'Act as if what you do makes a difference. It does.', user: 'William James' }
+]
+
+module.exports = messages
