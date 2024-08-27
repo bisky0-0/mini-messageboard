46292d0ec12ab0ed7a5020cce07aafa1d8f2d8cd
yasmeinelkenany <yasmeinelkenany@gmail.com>
2024-08-27 09:14:45 +0300

    fix: fixing host port

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config({ path: '/home/bisky/repos/node/TOP-FULLSTACK/mini-message-board/.env' });
 
 
 // Set up view engine and views folder
@@ -14,12 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 // Require and use routes
 const indexRoutes = require('./routes/index');
 const newRoutes = require('./routes/new');
-const { Console } = require('console');
+
 
 app.use('/', indexRoutes);
 app.use('/', newRoutes);
 
 // Start the server
-app.listen(8000, () => {
-    console.log("localhost is running");
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log("host is running");
 });
