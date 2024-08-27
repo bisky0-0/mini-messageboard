404901338fc35dd2497b3c0eb03c9e653858bf72
yasmeinelkenany <yasmeinelkenany@gmail.com>
2024-08-27 09:07:10 +0300

    delete: remove console.log from pool.js

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: '/home/bisky/repos/node/TOP-FULLSTACK/mini-message-board/.env' });
 const { Pool } = require('pg');
-console.log(process.env.USERNAME)
 module.exports = new Pool({
     host: process.env.HOST,
     user: process.env.USERNAME,
